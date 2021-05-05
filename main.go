8b16eb510b4fbe440f1bfe113280dbb670f19647
0xfederama <vgfede@gmail.com>
2021-05-05 03:55:35 +0200

    completed message handling, other small changes

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"math"
 	"math/rand"
+	"os"
+	"os/signal"
 	"strconv"
 	"strings"
 	"time"
@@ -15,17 +17,20 @@ import (
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/go-libp2p-pubsub"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/p2p/discovery"
 )
 
 //Used by mDNS ads to discover other peers
 const discoveryServiceTag = "bitcoin-simulation"
 
+//Used by the stream handler
+const protocolName = "/bitcoin-simulation/1.0.0"
+
 //Notified when a new peer is found via mDNS discovery
 type discoveryNotifee struct {
-	h   host.Host
-	ctx context.Context
+	host host.Host
+	ctx  context.Context
 }
 
 func main() {
@@ -35,7 +40,7 @@ func main() {
 	host, err := libp2p.New(
 		ctx,
 		libp2p.Defaults, ///ip4/0.0.0.0/tcp/0, /ip6/::/tcp/0, enable relay, /yamux/1.0.0, /mplex/6.7.0, tls, noise, tcp, ws, empty peerstore
-		//libp2p.EnableAutoRelay(), //TODO:
+		//libp2p.EnableAutoRelay(), //TODO: enable nat relay
 	)
 	if err != nil {
 		panic(err)
@@ -50,35 +55,59 @@ func main() {
 		panic(err)
 	}
 
-	//Setup local mDNS discovery
+	//Setup local mDNS discovery //TODO: change to rendezvous or kad-dht
 	err = setupDiscovery(ctx, host)
 	if err != nil {
 		panic(err)
 	}
 
 	//Join the topic
-	topicNetwork, err := JoinNetwork(ctx, ps, host.ID())
+	topicNet, err := JoinNetwork(ctx, host, ps, host.ID())
 	if err != nil {
 		panic(err)
 	}
 
+	//Goroutine to periodically send IHAVE messages
+	go periodicSendIHAVE(topicNet)
+
+	//Set stream handler to send and receive direct DATA and IWANT messages using the network struct
+	host.SetStreamHandler(protocolName, topicNet.handleStream)
+
+	//Wait for stop signal (Ctrl-C) and close the host
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	go func() {
+		<-stop
+		// topicNet.ps.UnregisterTopicValidator(topicName)
+		// topicNet.topic.Close()
+		// topicNet.sub.Cancel()
+		// host.Close()
+		fmt.Println("Exiting...")
+		os.Exit(0)
+	}()
+
 	//Loop to simulate creation of "transactions"
-	for {
-		peers := topicNetwork.ps.ListPeers(protocolName)
-		//log.Println(host.Peerstore().Peers())
-		log.Printf("- Found %d peers in the network: %s\n", len(peers), peers)
-		if PoW() {
-			//Create the block
-			content := strings.Repeat(strconv.FormatInt(int64(math.Pow(float64(time.Now().Unix()), 2)), 16), 16) //Use epoch to create a fake transaction
-			hash := md5.Sum([]byte(content))
-			header := hex.EncodeToString(hash[:])
-			//Store the block and publish it with an IHAVE message
-			topicNetwork.Blocks[header] = content
-			topicNetwork.Headers = append(topicNetwork.Headers, header)
-			log.Printf("- Created block with header %s and content %s\n", header, content)
-			topicNetwork.Publish(1, map[string]string{}, topicNetwork.Headers, []string{})
+	if true {
+		for i := 0; i < 2; i++ {
+			peers := topicNet.ps.ListPeers(topicName)
+			log.Printf("- Found %d other peers in the network: %s\n", len(peers), peers)
+			if PoW() {
+				//Create the block
+				content := strings.Repeat(strconv.FormatInt(int64(math.Pow(float64(time.Now().Unix()), 2)), 16), 8) //Use epoch to create a fake transaction
+				hash := md5.Sum([]byte(content))
+				header := hex.EncodeToString(hash[:])
+
+				//Store the block and publish it with an IHAVE message
+				topicNet.Blocks[header] = content
+				topicNet.Headers = append(topicNet.Headers, header)
+				log.Printf("- Created block with header %s and content %s\n", header, content)
+				if err := topicNet.Publish(topicNet.Headers); err != nil {
+					log.Println("- Error publishing IHAVE message on the network:", err)
+				}
+			}
+			time.Sleep(time.Second * 10) //Wait 10 seconds before computing another message
+			fmt.Println()
 		}
-		time.Sleep(time.Second * 20) //Wait 20 seconds before computing another message
 	}
 
 }
@@ -89,18 +118,18 @@ func setupDiscovery(ctx context.Context, h host.Host) error {
 	if err != nil {
 		return err
 	}
-	notifee := discoveryNotifee{h: h, ctx: ctx}
+	notifee := discoveryNotifee{host: h, ctx: ctx}
 	disc.RegisterNotifee(&notifee)
 	return err
 }
 
 //Connect to the newly discovered peer
 func (notifee *discoveryNotifee) HandlePeerFound(p peer.AddrInfo) {
-	if contains(notifee.h.Peerstore().Peers(), p.ID.Pretty()) { //Avoid saving, connecting to and printing the same peer twice
-		return
-	}
+	// if contains(notifee.h.Peerstore().Peers(), p.ID.Pretty()) { //Avoid saving, connecting to and printing the same peer twice
+	// 	return //FIXME: non funziona perche' comunque scopre e si connette spesso due volte insieme
+	// }
 	log.Println("- Discovered peer", p.ID)
-	err := notifee.h.Connect(notifee.ctx, p)
+	err := notifee.host.Connect(notifee.ctx, p)
 	if err != nil {
 		log.Printf("- Error connecting to %s: %s", p.ID, err)
 	} else {
@@ -108,11 +137,21 @@ func (notifee *discoveryNotifee) HandlePeerFound(p peer.AddrInfo) {
 	}
 }
 
+//Periodically send IHAVE messages on the newtork for newly entered peers
+func periodicSendIHAVE(net *TopicNetwork) {
+	for {
+		time.Sleep(time.Second * 30)
+		if err := net.Publish(net.Headers); err != nil {
+			log.Println("- Error publishing IHAVE message on the network:", err)
+		}
+	}
+}
+
 //Simulate proof of work with probability of creating the block
 func PoW() bool {
-	time.Sleep(time.Second * 3) //Simulate time used to compute the proof of work
+	time.Sleep(time.Second * 10) //Simulate time used to compute the proof of work
 	r := rand.Intn(10) + 1
-	if r > -1 { //FIXME: >3, testing //70% chance 
+	if r > -1 { //FIXME: >3, testing //70% chance
 		log.Println("- PoW succeeded")
 		return true
 	} else {
@@ -121,11 +160,11 @@ func PoW() bool {
 	}
 }
 
-func contains(addrs peer.IDSlice, p string) bool {
-	for _, addr := range addrs {
-		if addr.Pretty() == p {
-			return true
-		}
-	}
-	return false
-}
+// func contains(addrs peer.IDSlice, p string) bool {
+// 	for _, addr := range addrs {
+// 		if addr.Pretty() == p {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
--- protocol.go
+++ protocol.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 )
 
 const DEBUG = "\033[31m[*][DEBUG]\033[0m"
 
-const protocolName = "/bitcoin-simulation/1.0.0"
+const topicName = "/bitcoin-simulation/1.0.0"
 
 type TopicNetwork struct {
 	Messages chan *Message
@@ -20,6 +23,7 @@ type TopicNetwork struct {
 	Headers  []string
 
 	ctx   context.Context
+	host  host.Host
 	ps    *pubsub.PubSub
 	topic *pubsub.Topic
 	sub   *pubsub.Subscription
@@ -34,10 +38,11 @@ type Message struct {
 	IWANT   []string
 }
 
-func JoinNetwork(ctx context.Context, ps *pubsub.PubSub, self peer.ID) (*TopicNetwork, error) {
+//Join the GossipSub network
+func JoinNetwork(ctx context.Context, host host.Host, ps *pubsub.PubSub, self peer.ID) (*TopicNetwork, error) {
 
 	//Join the topic
-	topic, err := ps.Join(protocolName)
+	topic, err := ps.Join(topicName)
 	if err != nil {
 		return nil, err
 	}
@@ -50,6 +55,7 @@ func JoinNetwork(ctx context.Context, ps *pubsub.PubSub, self peer.ID) (*TopicNe
 
 	net := &TopicNetwork{
 		ctx:      ctx,
+		host:     host,
 		ps:       ps,
 		topic:    topic,
 		sub:      sub,
@@ -64,6 +70,7 @@ func JoinNetwork(ctx context.Context, ps *pubsub.PubSub, self peer.ID) (*TopicNe
 
 }
 
+//Loop service for GossipSub that handles IHAVE messages
 func (net *TopicNetwork) ReadService() {
 	for {
 
@@ -74,91 +81,166 @@ func (net *TopicNetwork) ReadService() {
 			return
 		}
 
-		//If I'm the sender, ignore it
+		//If I'm the sender, ignore the message
 		if received.ReceivedFrom == net.self {
 			log.Println("- I am the sender, ignoring the packet")
 			continue
 		}
 
-		//Get the message
+		//Unmarshal the message
 		message := new(Message)
 		err = json.Unmarshal(received.Data, message)
 		if err != nil {
 			continue
 		}
 
-		//Debug print to check if the message is parsed correctly
-		log.Println("- Received", string(received.Data))
-
 		printMessage(*message)
 
-		//Handle the message
-		switch message.MsgType {
-		case 0: //Store the data if I don't have the block
-			for header, content := range message.Blocks {
-				if _, found := net.Blocks[header]; !found {
-					net.Blocks[header] = content
-					log.Printf("- Stored block %s: %s\n", header, content)
-				} else {
-					log.Println("- I already have the message", header)
-				}
-			}
-		case 1: //Analyze IHAVE message to see if I need some blocks
+		//Handle the IHAVE message (IWANT and DATA are send directly, so GossipSub should not see them)
+		if message.MsgType == 1 {
 			iwant := make([]string, 0, 16)
 			for _, owned := range message.IHAVE {
 				if _, found := net.Blocks[owned]; !found {
 					iwant = append(iwant, owned)
 				}
 			}
-			//If I need some blocks, ask for them with an IWANT message
+			//If I need some blocks, ask for them with a direct IWANT message
 			if len(iwant) > 0 {
-				fmt.Println(DEBUG, fmt.Sprintf("I want these blocks from %s: %s", message.Sender, iwant))
-				//TODO: Send IWANT message directly to the peer
-			}
-			//Forward the IHAVE message in the network to see if someone else needs blocks listed here
-			net.Messages <- message
-			continue
-		case 2: //Analyze IWANT message to see if I can send the messages required
-			fmt.Println(DEBUG, fmt.Sprintf("Peer %s wants from me %s", message.Sender, message.IWANT))
-			toSend := make(map[string]string)
-			for _, wanted := range message.IWANT {
-				if block, found := net.Blocks[wanted]; found {
-					toSend[wanted] = net.Blocks[block]
+				msg := &Message{
+					MsgType: 2,
+					Sender:  net.self,
+					Blocks:  map[string]string{},
+					IHAVE:   []string{},
+					IWANT:   iwant,
 				}
+				net.directSend(message.Sender, *msg)
+			} else {
+				log.Println("- No blocks needed")
 			}
-			log.Printf("- I have to send these blocks to %s: %s", message.Sender, toSend)
-			//TODO: Send DATA message directly to the peer that requested it
-		default:
-			log.Fatalf("- Unsupported type of message: %d", message.MsgType)
+			//Forward the IHAVE message in the network to see if someone else needs the blocks listed here
+			log.Println("- Forwarding IHAVE message on the network")
+			net.Messages <- message
+		} else {
+			fmt.Println(DEBUG, "This was not expected, message type", message.MsgType)
 		}
 
-		// log.Println("Forwarding the message")
-		// net.Messages <- message
 	}
 }
 
-//Publishes the message as the original sender (not forwarding)
-func (net *TopicNetwork) Publish(msgType int, blocks map[string]string, ihave []string, iwant []string) error {
+//Publishes the message on the GossipSub network (used only for IHAVE messages)
+func (net *TopicNetwork) Publish(ihave []string) error {
 	message := &Message{
-		MsgType: msgType,
+		MsgType: 1,
 		Sender:  net.self,
-		Blocks:  blocks,
+		Blocks:  map[string]string{},
 		IHAVE:   ihave,
-		IWANT:   iwant,
+		IWANT:   []string{},
 	}
 	msg, err := json.Marshal(message)
 	if err != nil {
 		return err
 	}
-	log.Println("- Sending", string(msg))
+	log.Println("- Publishing", string(msg))
 	return net.topic.Publish(net.ctx, msg)
 }
 
+//Stream handler for DATA and IWANT messages
+func (net *TopicNetwork) handleStream(s network.Stream) {
+
+	log.Println("- Got a new stream")
+	rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
+	//Receive and respond message loop
+	go func(rw *bufio.ReadWriter) {
+		for {
+
+			//Get the message
+			read := make([]byte, 1024)
+			nRead, err := rw.Read(read)
+			read = read[:nRead]
+			if err != nil {
+				log.Println("- Error reading from the stream:", err)
+				return
+			}
+
+			//Unmarshal the message
+			message := new(Message)
+			err = json.Unmarshal(read, message)
+			if err != nil {
+				log.Println("- Error unmarshalling the received message:", err)
+				return
+			}
+
+			printMessage(*message)
+
+			//Handle the different messages
+			switch message.MsgType {
+			case 0: //Check the blocks received and store the ones not already stored
+
+				for header, content := range message.Blocks {
+					if _, found := net.Blocks[header]; !found {
+						net.Blocks[header] = content
+						net.Headers = append(net.Headers, header)
+						log.Printf("- Stored block %s: %s\n", header, content)
+					} else {
+						log.Println("- I already have the block", header)
+					}
+				}
+				fmt.Println(DEBUG, "Now I have these blocks:", net.Blocks)
+
+			case 2: //Send the blocks requested with a DATA message
+
+				//Create a map of requested block to send to the peer
+				toSend := make(map[string]string)
+				for _, wanted := range message.IWANT {
+					if _, found := net.Blocks[wanted]; found {
+						toSend[wanted] = net.Blocks[wanted]
+					}
+				}
+
+				//Send DATA message directly to the peer that requested those blocks
+				msg := &Message{
+					MsgType: 0,
+					Sender:  net.self,
+					Blocks:  toSend,
+					IHAVE:   []string{},
+					IWANT:   []string{},
+				}
+				net.directSend(message.Sender, *msg)
+
+			default:
+				log.Printf("- Message type %d received not supported for direct messaging", message.MsgType)
+			}
+		}
+	}(rw)
+
+}
+
 //Send directly from a peer to another
-// func directSend(sender peer.AddrInfo, receiver peer.AddrInfo, msg Message) error {
-// 	//TODO:
-// 	return nil
-// }
+func (net *TopicNetwork) directSend(receiver peer.ID, msg Message) {
+
+	//Open the stream to the receiver peer
+	stream, err := net.host.NewStream(net.ctx, receiver, protocolName)
+	if err != nil {
+		log.Printf("- Error opening stream to %s: %s\n", receiver, err)
+		return
+	}
+
+	//Marshal the message to send it
+	message, err := json.Marshal(msg)
+	if err != nil {
+		log.Println("- Error marshalling message to send:", err)
+		return
+	}
+
+	//Write the message on the stream
+	nWritten, err := stream.Write(message)
+	if err != nil {
+		log.Println("- Error sending message on stream:", err)
+		return
+	}
+	log.Printf("- Sent (%d chars) to %s: %s", nWritten, receiver, string(message))
+
+}
 
 /************ Support Functions ************/
 
@@ -169,6 +251,6 @@ func printMessage(msg Message) {
 	case 1:
 		log.Printf("- Message IHAVE received from %s, it has %s", msg.Sender, msg.IHAVE)
 	case 2:
-		log.Printf("- Message IWANT received from %s, it has %s", msg.Sender, msg.IWANT)
+		log.Printf("- Message IWANT received from %s, it wants %s", msg.Sender, msg.IWANT)
 	}
 }
